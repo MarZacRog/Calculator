af3528a253782b7d00856eef9c7627ade85bebf4
MarZacRog <marta.zaczyk@wp.pl>
2022-10-02 23:19:20 +0200

    Added method substraction, multiplication and division

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
        Calculator calc = new Calculator();
         System.out.println(calc.addition(4,6));
+        System.out.println(calc.substraction(10,2));
+        System.out.println(calc.multiplication(3,2));
+        System.out.println(calc.division(10,2));
         }}
